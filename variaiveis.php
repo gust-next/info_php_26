78f75bfbde61dd011b3ea848faf0b9e6f523ebe1
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-03-17 21:38:35 -0300

    operadores aritimeticos e de concatenacao mais comando echo

--- variaiveis.php
+++ variaiveis.php
@@ -1,6 +1,54 @@
 <?php
 
-$numero = 10;
+// 
 
-echo $numero;
-// echo 10;
+// Mock/Chumbar/fixar = ler dados usuario
+$nome = "Gustavo";
+$idade = "22";
+$sexo = "masculino";
+$nomePai = "Adelar";
+$nomeMae = "Claudia";
+echo "O usuario de nome $nome, $idade, $sexo é filho de $nomePai e $nomeMae";
+
+/** Ler os dados de usuarios, sendo eles:
+ * nome
+ * idade
+ * sexo
+ * nome pai
+ * nome mae
+ * E escrever a frase: O Usuario de NOME, IDADE, SEXO é filho(a) de:
+ * Nome pai e nome mae
+ */
+
+/**
+ * Somar dois valores e exibir o resultado dos mesmos com a frase:
+ * A soma dos valores é: SOMA
+ */
+
+/**
+* Subtrair dois valores e exibir o resultado dos mesmos com a frase:
+ * A subtração dos valores é: SUBTRAÇÃO
+ */
+
+/** 
+ * Multiplicar dois valores e exibir o resultado dos mesmos com a frase:
+ * A Multiplicação dos valores é: MULTPLICAÇÃ
+*/
+
+/**
+ * Dividir dois valores e exibir o resultado dos mesmos com a frase:
+ * A divisão dos valores é: Divisão
+ */
+
+$Valor1 = 2;
+$Valor2 = 3;
+
+$resultadoSoma = $Valor1 + $Valor2; // Soma
+$resultadoSub = $Valor1 - $Valor2; // Subtração
+$resultadoMult = $Valor1 * $Valor2; // Multiplicação
+$resultadoDivi = $Valor1 = $Valor2; // Divisão
+
+echo "A soma dos valores é: $resultadoSoma + $Valor2";
+echo "A soma dos valores é: $resultadoSub - $Valor2";
+echo "A soma dos valores é: $resultadoMult * $Valor2";
+echo "A soma dos valores é: $resultadoDivi = $Valor2";
